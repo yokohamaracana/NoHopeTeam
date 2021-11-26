6c34a15f5d6c46eca0bc0f7bad25dbcf00498424
Phuc Toan <phuctoan18@gmail.com>
2021-11-26 18:38:57 +0700

    Thêm dữ liệu vào database

---
+++ src/main/java/com/mycompany/qlsvapp/model/dao/BangDiemDao.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.qlsvapp.model.dao;
+
+import com.mycompany.qlsvapp.helper.Database;
+import com.mycompany.qlsvapp.model.BangDiem;
+import com.mycompany.qlsvapp.model.SinhVien;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.rowset.serial.SerialBlob;
+
+/**
+ *
+ * @author Phuc Toan, Trung Hau, Duc Duy, Anh Tri
+ */
+public class BangDiemDao {
+    public boolean insert(BangDiem bd) throws Exception{
+        
+        String sql = "INSERT INTO [dbo].[BangDiem]([MaSinhVien],[Toan],[NgoaiNgu],[TinHoc],[TrietHoc]) values(?,?,?,?,?)";
+        try (
+            Connection con = Database.openConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ){
+            pstmt.setString(1, bd.getMaSinhVien());
+            pstmt.setFloat(2, bd.getToan());
+            pstmt.setFloat(3, bd.getNgoaiNgu());
+            pstmt.setFloat(4, bd.getTinHoc());
+            pstmt.setFloat(5, bd.getTrietHoc());
+            
+            return pstmt.executeUpdate()>0;
+            
+        }
+    }
+    
+}
