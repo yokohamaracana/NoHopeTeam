e3124ab84c383fd312937ee9735c37eb606400e4
Tuan Tien <tuantien2k1@gmail.com>
2021-11-26 17:58:52 +0700

    Share data

---
+++ src/main/java/com/mycompany/qlsvapp/helper/SharedData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.qlsvapp.helper;
+
+import com.mycompany.qlsvapp.model.NguoiDung;
+
+/**
+ *
+ * @author Tuan Tien
+ */
+public class SharedData {
+    public static NguoiDung nguoiDangNhapDung;
+}
